36b2d132282e8cfeb1bc7d1519113ac9eb6666d4
babooloon <nickyangcchs@hotmail.com>
2017-07-11 16:19:37 +0800

    Modify author info.
    Signed-off-by: babooloon <nickyangcchs@hotmail.com>

--- InsertionSort/src/InsertionSort.java
+++ InsertionSort/src/InsertionSort.java
@@ -1,6 +1,7 @@
 /**
  * Created by babooloon on 2017/7/11.
  * Email: nickyangcchs@hotmail.com
+ * Project: SortingAlgorithm
  */
 class InsertionSort {
     static void insertionSort(int[] a, int start, int end) {
--- QuickSort/src/QuickSort.java
+++ QuickSort/src/QuickSort.java
@@ -1,6 +1,7 @@
 /**
  * Created by babooloon on 2017/7/11.
  * Email: nickyangcchs@hotmail.com
+ * Project: SortingAlgorithm
  */
 public class QuickSort {
 }
